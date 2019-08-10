cd3b4c50be2e11b0604b66452125369f0eb8e246
Stefan Preusler <stefan@preusler.de>
2019-08-10 22:38:57 +0200

    Change comments to EN

--- face-recognition.py
+++ face-recognition.py
@@ -1,21 +1,20 @@
 import cv2
 import sys
 
-# Laden des bereits trainierten Modells 'haarcascade_frontalface_alt.xml"
-# zur Klassifikation / Erkennung von Gesichtern
+# Load pre-trained model 'haarcascade_frontalface_alt.xml" for face recognition / classification
 faceCascade = cv2.CascadeClassifier('haarcascade_frontalface_alt.xml')
 
-# Erzeugung eines Objekts, das auf die Default Kamera (Webcam Parameter 0) zugreift
+# Create object for default camera 0
 video_capture = cv2.VideoCapture(0)
 
 while True:
-    # Aufgreifen des Bildes pro Frame
+    # Get frame from video
     ret, frame = video_capture.read()
 
-    # Zuweisung der Farbe grau fuer Box Umrandung des Gesichts
+    # Define grey for face box
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    # Erkennung mehrerer Gesichter
+    # Detect faces
     faces = faceCascade.detectMultiScale(
         gray,
         scaleFactor=1.1,
@@ -24,18 +23,17 @@ while True:
         flags=cv2.CASCADE_SCALE_IMAGE
     )
 
-    # Erzeugen eines Rechtecks fuer jedes der erkannten Gesichter und anfuegen in Frame
+    # Append rectangle to each face in a frame
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
 
-    # Ausgabe der Frames
+    # Show frame as video stream
     cv2.imshow('Video', frame)
 
-    # Solange laufen bis 'q' gedrueckt laeuft das Programm. Ansonsten springt das
-    # Programm mit 'break' aus while-Schleife
+    # Run program until user enters 'q'
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Sofern Programm aus while-Schleife springt, erfolgt die Freigabe des Objekts und Beendigung aller Fenster
+# If program exists while loop, release video capture and close all windows
 video_capture.release()
 cv2.destroyAllWindows()
